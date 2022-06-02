b2b7c3ebc308d6950dc2c9114ab3a025fb10acfd
htkelly <hrykly@gmail.com>
2022-06-03 00:52:32 +0100

    Added password hashing and salting, modified seed data to include hashed passwords

--- src/controllers/accounts-controller.js
+++ src/controllers/accounts-controller.js
@@ -1,5 +1,8 @@
+import bcrypt from "bcrypt";
 import { db } from "../models/db.js";
 import { UserSpec, UserCredentialsSpec } from "../models/joi-schemas.js";
+
+const saltRounds = 10;
 
 export const accountsController = {
   index: {
@@ -27,6 +30,7 @@ export const accountsController = {
     },
     handler: async function (request, h) {
       const user = request.payload;
+      user.password = await bcrypt.hash(user.password, saltRounds);
       await db.userStore.addUser(user);
       return h.redirect("/");
     },
@@ -51,7 +55,8 @@ export const accountsController = {
     handler: async function (request, h) {
       const { email, password } = request.payload;
       const user = await db.userStore.getUserByEmail(email);
-      if (!user || user.password !== password) {
+      const passwordsMatch = await bcrypt.compare(password, user.password);
+      if (!user || !passwordsMatch) {
         return h.redirect("/");
       }
       request.cookieAuth.set({ id: user._id });
